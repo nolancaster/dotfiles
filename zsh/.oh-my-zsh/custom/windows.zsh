6a32fd211fc87f0da6bd383f303e611feb0956f6
Nolan Lancaster <nolancaster@gmail.com>
2017-07-23 00:02:13 -0400

    add some config for wsl

---
+++ zsh/.oh-my-zsh/custom/windows.zsh
@@ -0,0 +1,7 @@
+eval "$(dircolors ~/.dircolors)";
+
+export PATH=$PATH:/mnt/c/HashiCorp/Vagrant/bin
+alias vagrant=vagrant.exe
+
+export PATH=$PATH:"/mnt/c/Program Files/Sublime Text 3/"
+alias subl=subl.exe
